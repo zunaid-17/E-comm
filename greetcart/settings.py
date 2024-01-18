3bcd95e6cac42b8559221ecae030a0a88776bc25
zunaid-17 <zunaidkas69@gmail.com>
2024-01-18 14:18:15 +0530

    banner and categories

--- greetcart/settings.py
+++ greetcart/settings.py
@@ -145,7 +145,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'zunaidkas69@gmail.com'
-EMAIL_HOST_PASSWORD = 'wovo lbbr xbes shrx'
+EMAIL_HOST_PASSWORD = 'gdmc wtgb rqud qiis'
 EMAIL_USE_TLS = True
 
 
